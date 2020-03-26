f11938b0d2b6b1d01c3a2a575798943a61128d34
snagwuk <snagwuk@gmail.com>
2020-03-26 19:47:02 +0900

    cash 기능 구현
    cash 충전, 출금 , 나의 캐쉬내역 
    추가해야될 사항 : 관리자승인관련, ID 업데이트

---
+++ src/main/java/controller/CashController.java
@@ -0,0 +1,89 @@
+package controller;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import model.Cash;
+import service.MybatisCashDao;
+
+@Controller
+public class CashController
+{
+    
+    @Autowired
+    MybatisCashDao dbPro;
+    
+    @RequestMapping(value = "charge", method = RequestMethod.GET)
+    public String cash_chargeForm(Model m)
+    {
+        String id = "psw";
+        int mycash = dbPro.myCash(id);
+        m.addAttribute("mycash", mycash);
+        return "pay/charge";
+    }
+    
+    @RequestMapping(value = "charge", method = RequestMethod.POST)
+    public String cash_chargePro(Cash cash) throws Exception
+    {
+        cash.setCashdate(LocalDateTime.now());
+        cash.setReason("캐쉬충전");
+        System.out.println(cash);
+        dbPro.insertCash(cash);
+        return "redirect:/mypage";
+    }
+    
+    @RequestMapping(value = "withdraw", method = RequestMethod.GET)
+    public String cash_withdrawForm(Model m)
+    {
+        // id
+        String id = "psw";
+        int mycash = dbPro.myCash(id);
+        m.addAttribute("mycash", mycash);
+        return "pay/withdraw";
+    }
+    
+    @RequestMapping(value = "withdraw", method = RequestMethod.POST)
+    public String cash_withdrawPro(Cash cash) throws Exception
+    {
+
+        cash.setCashdate(LocalDateTime.now());
+        cash.setReason("캐쉬출금");
+        int minuscash = 0 - cash.getCash();
+        cash.setCash(minuscash);
+        System.out.println(cash);
+        dbPro.insertCash(cash);
+        return "redirect:/mypage";
+    }
+    
+    @RequestMapping(value = "cashlist", method = RequestMethod.GET)
+    public String cash_cashlist(Model m)
+    {
+
+        // id
+        String id = "psw";
+        
+        int mycash = dbPro.myCash(id);
+        m.addAttribute("mycash", mycash);
+        
+        int count = dbPro.getMyCashCount(id);
+        m.addAttribute("count", count);
+        
+        System.out.println(count);
+        if(count != 0)
+        {
+            List<Cash> cashlist = dbPro.getMyCashList(id);
+            m.addAttribute("cashlist", cashlist);
+        }
+
+        return "pay/cashlist";
+    }
+    
+    
+}
